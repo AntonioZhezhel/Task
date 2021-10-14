d6aecbdaba04d452720fa29ce31d1f4a7ac20bfa
AntonioZhezhel <42389663+AntonioZhezhel@users.noreply.github.com>
2021-10-14 23:15:53 +0300

    Update Lessons5_6_Queue.cs

--- Lessons5_6_Queue.cs
+++ Lessons5_6_Queue.cs
@@ -6,68 +6,85 @@ namespace Lessons5_6_Queue
     {
         static void Main(string[] args)
         {
-            Enqueue(10);
-            Enqueue(20);
-            Enqueue(30);
-            Enqueue(40);
-            Enqueue(50);
-            Enqueue(60);
-            Print();
-            Dequeue();
-            Enqueue(70);
-            Print();
-            Dequeue();
-            Print();
+            Operations operations = new Operations();
+            Queue queue = new Queue();
+
+            operations.Enqueue(queue, 10);
+            operations.Enqueue(queue, 20);
+            operations.Enqueue(queue, 30);
+            operations.Enqueue(queue, 40);
+            operations.Enqueue(queue, 50);
+            operations.Enqueue(queue, 60);
+            operations.Print(queue);
+            operations.Dequeue(queue);
+            operations.Enqueue(queue, 70);
+            operations.Print(queue);
+            operations.Dequeue(queue);
+            operations.Print(queue);
         }
 
-        public static void Enqueue(int item)
+        public class Operations
         {
-            Node newNode = new Node(item);
-            if (Queue.rear == null)
+            public void Enqueue(Queue queue, int item)
             {
-                Queue.front = Queue.rear = newNode;
-            }
-            else
-            {
-                Queue.rear.next = newNode;
-                Queue.rear = newNode;
+                Node newNode = new Node(item);
+                if (Queue.rear == null)
+                {
+                    Queue.front = Queue.rear = newNode;
+                }
+                else
+                {
+                    Queue.rear.next = newNode;
+                    Queue.rear = newNode;
+                }
+
+                Console.WriteLine("{0} in Queue ", item);
             }
 
-            Console.WriteLine("{0} in Queue ", item);
+            public void Dequeue(Queue queue)
+            {
+                if (Queue.front == null)
+                {
+                    Console.WriteLine("error");
+                }
+
+                Node temp = Queue.front;
+                Queue.front = Queue.front.next;
+                if (Queue.front == null)
+                {
+                    Queue.rear = null;
+                }
+
+                Console.WriteLine("deleted is{0}", temp.data);
+            }
+
+            public void Print(Queue queue)
+            {
+                if (Queue.front == null)
+                {
+                    Console.WriteLine("The Queue is empty");
+                }
+
+                Node temp = Queue.front;
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp = temp.next;
+                }
+
+                Console.WriteLine("--------------");
+            }
         }
+    }
 
-        public static void Dequeue()
+    public class Queue
+    {
+        public static Node front;
+        public static Node rear;
+
+        public Queue()
         {
-            if (Queue.front == null)
-            {
-                Console.WriteLine("error");
-            }
-
-            Node temp = Queue.front;
-            Queue.front = Queue.front.next;
-            if (Queue.front == null)
-            {
-                Queue.rear = null;
-            }
-
-            Console.WriteLine("deleted is{0}", temp.data);
-        }
-
-        public static void Print()
-        {
-            if (Queue.front == null)
-            {
-                Console.WriteLine("The Queue is empty");
-            }
-
-            Node temp = Queue.front;
-            while (temp != null)
-            {
-                Console.WriteLine(temp.data);
-                temp = temp.next;
-            }
-
-            Console.WriteLine("--------------");
+            front = rear = null;
         }
     }
 
@@ -80,17 +97,6 @@ namespace Lessons5_6_Queue
         {
             data = d;
             next = null;
-        }
-    }
-
-    public class Queue
-    {
-        public static Node front;
-        public static Node rear;
-
-        public Queue()
-        {
-            front = rear = null;
         }
     }
 }
