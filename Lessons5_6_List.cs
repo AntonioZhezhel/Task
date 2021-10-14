f1a25bb84b11face0531ee2a64c66810f452b648
AntonioZhezhel <42389663+AntonioZhezhel@users.noreply.github.com>
2021-10-14 22:58:03 +0300

    Update Lessons5_6_List.cs

--- Lessons5_6_List.cs
+++ Lessons5_6_List.cs
@@ -6,75 +6,81 @@ namespace Lessons5_6_List
     {
         static void Main(string[] args)
         {
+            Operations operations = new Operations();
             List list = new List();
 
-            Add(list, 10);
-            Add(list, 12);
-            Add(list, 13);
-            Add(list, 14);
-            Print(list);
+            operations.Add(list, 10);
+            operations.Add(list, 12);
+            operations.Add(list, 13);
+            operations.Add(list, 14);
+            operations.Print(list);
             Console.WriteLine("---------------------");
-            DeleteByKey(list, 12);
-            Print(list);
+            operations.DeleteByKey(list, 12);
+            operations.Print(list);
             Console.WriteLine("---------------------");
-            Search(list, 55);
-            Search(list, 13);
+            operations.Search(list, 55);
+            operations.Search(list, 13);
         }
 
-        public static void Add(List list, int newData)
+        public class Operations
         {
-            Node newNode = new Node(newData);
-            newNode.next = list.head;
-            list.head = newNode;
-        }
-
-        public static void DeleteByKey(List list, int key)
-        {
-            Node temp = list.head;
-            Node prev = null;
-            if (temp != null && temp.data == key)
+            public void Add(List list, int newData)
             {
-                list.head = temp.next;
+                Node newNode = new Node(newData);
+                newNode.next = list.head;
+                list.head = newNode;
             }
 
-            while (temp != null && temp.data != key)
+            public void DeleteByKey(List list, int key)
             {
-                prev = temp;
-                temp = temp.next;
-            }
-
-            if (temp == null)
-            {
-                return;
-            }
-
-            prev.next = temp.next;
-        }
-
-        public static void Search(List list, int value)
-        {
-            Node temp = list.head;
-            while (temp != null)
-            {
-                if (temp.data == value)
+                Node temp = list.head;
+                Node prev = null;
+                if (temp != null && temp.data == key)
                 {
-                    Console.WriteLine("element{0}", value);
+                    list.head = temp.next;
+                }
+
+                while (temp != null && temp.data != key)
+                {
+                    prev = temp;
+                    temp = temp.next;
+                }
+
+                if (temp == null)
+                {
                     return;
                 }
 
-                temp = temp.next;
+                prev.next = temp.next;
             }
 
-            Console.WriteLine("error");
-        }
-
-        public static void Print(List list)
-        {
-            Node node = list.head;
-            while (node != null)
+            public void Search(List list, int value)
             {
-                Console.WriteLine(node.data);
-                node = node.next;
+                Node temp = list.head;
+                while (temp != null)
+
+
+                {
+                    if (temp.data == value)
+                    {
+                        Console.WriteLine("element{0}", value);
+                        return;
+                    }
+
+                    temp = temp.next;
+                }
+
+                Console.WriteLine("error");
+            }
+
+            public void Print(List list)
+            {
+                Node node = list.head;
+                while (node != null)
+                {
+                    Console.WriteLine(node.data);
+                    node = node.next;
+                }
             }
         }
     }
